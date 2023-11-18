7117ef2a2d2d318827cb3971bff1b4181a47d9d1
Szymon Kozioł <szymon.koziol@deployed.pl>
2023-11-18 22:53:58 +0100

    Texture assets

---
+++ src/textures/images.ts
@@ -0,0 +1,7 @@
+import dirtTexture from './assets/dirt.jpg';
+import glassTexture from './assets/glass.png';
+import grassTexture from './assets/grass.jpg';
+import logTexture from './assets/log.jpg';
+import woodTexture from './assets/wood.png';
+
+export { dirtTexture, glassTexture, grassTexture, logTexture, woodTexture };

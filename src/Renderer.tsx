cc05952365dbfafb6ce54e8219abcdc42acf789f
Szymon Kozioł <szymon.koziol@deployed.pl>
2023-11-18 23:13:31 +0100

    Pointer lock controls

--- src/Renderer.tsx
+++ src/Renderer.tsx
@@ -1,8 +1,12 @@
+import { Sky } from '@react-three/drei';
+import { PointerLockControls } from '@react-three/drei';
+
 export function Renderer() {
   return (
     <>
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
+      <Sky sunPosition={[100, 20, 100]} />
+      <ambientLight intensity={0.5} />
+      <PointerLockControls />
       <mesh>
         <boxGeometry />
         <meshStandardMaterial />
